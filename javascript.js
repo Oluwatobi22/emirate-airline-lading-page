32a638779e1ede388939aca3cc25879da87b727b
Oluwatobi22 <bakareoluwatobi22@gmail.com>
2022-06-04 00:22:53 +0100

    emirate-airline landing-page

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function showSearch(){
+    searchC = document.getElementById("searchFlight");
+    document.getElementById('filler').innerHTML = searchC.innerHTML
+}
+function showManage(){
+    searchC = document.getElementById("manage");
+    document.getElementById('filler').innerHTML = searchC.innerHTML
+}
+function showflight(){
+    searchC = document.getElementById("what");
+    document.getElementById('filler').innerHTML = searchC.innerHTML
+}
+function status(){
+    searchC = document.getElementById("status");
+    document.getElementById('filler').innerHTML = searchC.innerHTML
+}
